54eed49b0f5b4b8d50b64e12472d0cf3ac50172b
Mathesh <mathesh1907@gmail.com>
2025-02-10 14:55:44 +0530

    minor updates

--- book-network/src/main/java/com/mugiwara/book/security/JwtFilter.java
+++ book-network/src/main/java/com/mugiwara/book/security/JwtFilter.java
@@ -1,34 +1,64 @@
 package com.mugiwara.book.security;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.security.Key;
-import java.util.Base64;
+import java.io.IOException;
 
-public class JWTVerificationExample {
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
-    private static final String SECRET_KEY = "3D77C395F14D4230A75FE34BA2BC6A66E6984516C039D21CBDA6JEEVASOROHINIM6BC6D22CDAB44";
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
 
-    public static void main(String[] args) {
-        String jwtToken = "eyJhbGciOiJIUzM4NCJ9.eyJmdWxsTmFtZSI6IlRvbnkgQ2hvcHBlciIsInN1YiI6ImFsdC5nMi0zaXF0MTJsQHlvcG1haWwuY29tIiwiaWF0IjoxNzM4OTM4MDEzLCJleHAiOjE3MzkwMjQ0MTMsImF1dGhvcml0aWVzIjpbIlVTRVIiXX0.VOplLPzTqSnMYYhmDJLSS-MBYoQG--BJafCbM1TnQOA1ZCTVk0ZEmi6O3iEwNKep";
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
-        try {
-            Claims claims = decodeJWT(jwtToken);
-            System.out.println("Token is valid!");
-            System.out.println("Claims: " + claims);
-        } catch (Exception e) {
-            System.out.println("Invalid token: " + e.getMessage());
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        if (request.getServletPath().contains("/api/v1/auth")) {
+            filterChain.doFilter(request, response);
+            return;
         }
-    }
+        final String authHeader = request.getHeader(AUTHORIZATION);
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
-    private static Claims decodeJWT(String jwt) {
-        Key key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(SECRET_KEY));
-        return Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(jwt)
-                .getBody();
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUserName(jwt);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 }
